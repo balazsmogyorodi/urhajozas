0d4d04f74e147d2b731054dfd1a321b31bc0d8c2
Martonnvok <martonsom@gmail.com>
2023-09-29 12:11:26 +0200

    Űrhajós tábla feltöltése

--- database/migrations/2023_09_23_124409_create_urhajos_table.php
+++ database/migrations/2023_09_23_124409_create_urhajos_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Urhajos;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,44 @@ return new class extends Migration
             $table->foreignId('urhajoId')->references('id')->on('Urhajo');
             $table->timestamps();
         });
+
+        Urhajos::create([
+            'azonosito' => 1,
+            'nev' => 'Nevem Álmos',
+            'szuletes' => '2002.06.02'
+        ]);
+        Urhajos::create([
+            'azonosito' => 2,
+            'nev' => 'Nevem Előd',
+            'szuletes' => '2002.06.02'
+        ]);
+        Urhajos::create([
+            'azonosito' => 3,
+            'nev' => 'Nevem Huba',
+            'szuletes' => '2002.06.02'
+        ]);
+
+        Urhajos::create([
+            'azonosito' => 4,
+            'nev' => 'Nevem Kun',
+            'szuletes' => '2002.06.02'
+        ]);
+        Urhajos::create([
+            'azonosito' => 5,
+            'nev' => 'Nevem Ond',
+            'szuletes' => '2002.06.02'
+        ]);
+        Urhajos::create([
+            'azonosito' => 6,
+            'nev' => 'Nevem Tas',
+            'szuletes' => '2002.06.02'
+        ]);
+
+        Urhajos::create([
+            'azonosito' => 6,
+            'nev' => 'Nevem Töhötöm',
+            'szuletes' => '2002.06.02'
+        ]);
     }
 
     /**
